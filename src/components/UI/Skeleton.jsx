35fb446542ddee039d4f65db6331a6bec0de4a65
Sam <samarooniii@yahoo.com>
2023-02-15 12:38:05 -0500

    top sellers DONE

--- src/components/UI/Skeleton.jsx
+++ src/components/UI/Skeleton.jsx
@@ -3,36 +3,29 @@ import { Card, CardHeader, Skeleton as Skelly } from "@mui/material";
 
 const Skeleton = ({ loading, sellers }) => {
   return (
-    <Card sx={{ maxWidth: 345, m: 5 }}>
-      <CardHeader
-        style={{ border: "1px solid white" }}
-        avatar={
-          loading ? (
-            <Skelly
-              variant="circular"
-              width={40}
-              height={40}
-              animation="wave"
-            />
-          ) : (
-            <img src={sellers.authorImage} alt="" />
-          )
-        }
-        title={
-          loading ? (
-            <>
-              <Skelly animation="wave" height={20} width="80%" />
-              <Skelly animation="wave" height={20} width="40%" />
-            </>
-          ) : (
-            <>
-              <span>{sellers.authorName}</span>
-              <span>{sellers.price}</span>
-            </>
-          )
-        }
-      ></CardHeader>
-    </Card>
+    <CardHeader
+      style={{ border: "1px solid white" }}
+      avatar={
+        loading ? (
+          <Skelly variant="circular" width={40} height={40} animation="wave" />
+        ) : (
+          <img src={sellers.authorImage} alt="" />
+        )
+      }
+      title={
+        loading ? (
+          <>
+            <Skelly animation="wave" height={20} width="80%" />
+            <Skelly animation="wave" height={20} width="40%" />
+          </>
+        ) : (
+          <>
+            <span>{sellers.authorName}</span>
+            <span>{sellers.price}</span>
+          </>
+        )
+      }
+    ></CardHeader>
   );
 };
 

b38ff90812cd455edec39d2a025c138b27bcce34
Sam <samarooniii@yahoo.com>
2023-02-18 22:53:23 -0500

    idk

--- src/components/UI/Skeleton.jsx
+++ src/components/UI/Skeleton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card, CardHeader, Skeleton as Skelly } from "@mui/material";
+import "../../SkeletonTwo.css";
 
 const Skeleton = ({ loading, sellers }) => {
   return (

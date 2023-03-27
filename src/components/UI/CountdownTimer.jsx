fa2e394074cf3ab40adca2f78c9dbc392193c4e1
Sam <samarooniii@yahoo.com>
2023-03-26 20:59:41 -0400

    LAST PUSH LOL

--- src/components/UI/CountdownTimer.jsx
+++ src/components/UI/CountdownTimer.jsx
@@ -6,7 +6,7 @@ export const CountdownTimer = ({ targetDate }) => {
   const [hours, minutes, seconds] = useCountdown(targetDate);
 
   return (
-    <div className="d-flex justify-content-end position-sticky">
+    <div className="d-flex justify-content-end">
       <ShowCounter hours={hours} minutes={minutes} seconds={seconds} />
     </div>
   );
